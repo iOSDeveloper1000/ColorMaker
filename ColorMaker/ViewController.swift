f94e7cfa60a625cf1bedc3af0f67fa2e122e9faf
iOSDeveloper1000 <aseidel-it@posteo.de>
2020-10-20 13:01:37 +0200

    Implemented ColorMaker app for mixing rgb values to depict on app screen

---
+++ ColorMaker/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by Arno Seidel on 20.10.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func changeColorComponent(_ sender: AnyObject) {
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+
+}
+
